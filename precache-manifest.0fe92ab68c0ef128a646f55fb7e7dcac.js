13a16a0ebb63369e5658c0e9061487431f0d5168
alexteo <alex.teodor87@yahoo.com>
2019-01-22 19:54:54 +0200

    Updates

---
+++ precache-manifest.0fe92ab68c0ef128a646f55fb7e7dcac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/alexteo/rsvp_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "be92ab00f0f555419adb491aa392b9ca",
+    "url": "/alexteo/rsvp_app/static/media/header-bg.be92ab00.jpg"
+  },
+  {
+    "revision": "bbcc224017872eb212cc",
+    "url": "/alexteo/rsvp_app/static/js/runtime~main.bbcc2240.js"
+  },
+  {
+    "revision": "6a3cfd42c9ae5fddb400",
+    "url": "/alexteo/rsvp_app/static/js/main.6a3cfd42.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/alexteo/rsvp_app/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "6a3cfd42c9ae5fddb400",
+    "url": "/alexteo/rsvp_app/static/css/main.2f308363.chunk.css"
+  },
+  {
+    "revision": "9a667476b4a21a4e775148f88e08a8ba",
+    "url": "/alexteo/rsvp_app/index.html"
+  }
+];
